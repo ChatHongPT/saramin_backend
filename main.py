85523631252467d4521c1bdeca674c866d582850
suk924600 <suk924600@naver.com>
2024-12-11 20:22:47 +0900

    fix: 메인 실행 파일(블루 프린트 등록)

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from app.routes.api import app
+from flask import Flask
+from app.routes import auth, jobs, apps, bookmarks, resumes
+
+app = Flask(__name__)
+
+# 블루프린트 등록
+app.register_blueprint(auth.bp, url_prefix="/auth")
+app.register_blueprint(jobs.bp, url_prefix="/jobs")
+app.register_blueprint(apps.bp, url_prefix="/apps")
+app.register_blueprint(bookmarks.bp, url_prefix="/bookmarks")
+app.register_blueprint(resumes.bp, url_prefix="/resumes")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
