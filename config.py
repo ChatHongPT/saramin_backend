347e07288890e997c32fe444e95d93ecf7de7448
suk924600 <suk924600@naver.com>
2024-12-12 19:53:39 +0900

    feat: 환경설정

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
-# config.py
+# MongoDB 설정
+MONGO_URI = "mongodb://localhost:27017/"
 
-import os
+# PostgreSQL 설정
+POSTGRES_CONFIG = {
+    "dbname": "job_database",
+    "user": "username",
+    "password": "password",
+    "host": "localhost",
+    "port": 5432
+}
 
-class Config:
-    # MongoDB 연결 정보
-    MONGODB_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
-    DATABASE_NAME = os.getenv('DATABASE_NAME', 'job_data')
+# Redis 설정
+REDIS_CONFIG = {
+    "host": "localhost",
+    "port": 6379,
+    "db": 0
+}
