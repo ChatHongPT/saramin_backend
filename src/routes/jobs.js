6a6803c0035f4d1b703e53124dec5207490feedb
suk924600 <suk924600@naver.com>
2024-12-14 14:06:43 +0900

    Update: jobs.js 로직 수정

--- src/routes/jobs.js
+++ src/routes/jobs.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { JobController } from '../controllers/jobController.js';
 import { authenticate } from '../middleware/auth.js';
-import { validateJobSearch, validateJobFilters } from '../middleware/validators/jobValidators.js';
+import { validateJobSearch } from '../middleware/validators/jobValidators.js';
 import { asyncWrapper } from '../middleware/asyncWrapper.js';
 
 export const router = express.Router();
@@ -9,8 +9,6 @@ const jobController = new JobController();
 
 // Public routes
 router.get('/', validateJobSearch, asyncWrapper(jobController.getJobs));
-router.get('/search', validateJobSearch, asyncWrapper(jobController.searchJobs));
-router.get('/filter', validateJobFilters, asyncWrapper(jobController.filterJobs));
 router.get('/:id', asyncWrapper(jobController.getJobById));
 
 // Protected routes
