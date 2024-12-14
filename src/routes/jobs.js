e44f35d55936b01a1a77394f81242046ea41a9b7
suk924600 <suk924600@naver.com>
2024-12-14 13:52:04 +0900

    Update: jobs.js 로직 수정

--- src/routes/jobs.js
+++ src/routes/jobs.js
@@ -7,27 +7,16 @@ import { asyncWrapper } from '../middleware/asyncWrapper.js';
 export const router = express.Router();
 const jobController = new JobController();
 
-// Wrap controller methods with asyncWrapper
-const wrappedController = {
-  getJobs: asyncWrapper(jobController.getJobs),
-  searchJobs: asyncWrapper(jobController.searchJobs),
-  getJobById: asyncWrapper(jobController.getJobById),
-  filterJobs: asyncWrapper(jobController.filterJobs),
-  bookmarkJob: asyncWrapper(jobController.bookmarkJob),
-  removeBookmark: asyncWrapper(jobController.removeBookmark),
-  getBookmarks: asyncWrapper(jobController.getBookmarks),
-};
-
 // Public routes
-router.get('/', validateJobSearch, wrappedController.getJobs);
-router.get('/search', validateJobSearch, wrappedController.searchJobs);
-router.get('/filter', validateJobFilters, wrappedController.filterJobs);
-router.get('/:id', wrappedController.getJobById);
+router.get('/', validateJobSearch, asyncWrapper(jobController.getJobs));
+router.get('/search', validateJobSearch, asyncWrapper(jobController.searchJobs));
+router.get('/filter', validateJobFilters, asyncWrapper(jobController.filterJobs));
+router.get('/:id', asyncWrapper(jobController.getJobById));
 
 // Protected routes
 router.use(authenticate);
-router.post('/:id/bookmark', wrappedController.bookmarkJob);
-router.delete('/:id/bookmark', wrappedController.removeBookmark);
-router.get('/bookmarks/list', wrappedController.getBookmarks);
+router.post('/:id/bookmark', asyncWrapper(jobController.bookmarkJob));
+router.delete('/:id/bookmark', asyncWrapper(jobController.removeBookmark));
+router.get('/bookmarks/list', asyncWrapper(jobController.getBookmarks));
 
 export default router;
