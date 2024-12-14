4f052515d2470df50db56f770b068282e57c6234
suk924600 <suk924600@naver.com>
2024-12-14 14:05:19 +0900

    Update: searchHistory 라우터 추가

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,6 +5,7 @@ import { router as jobRouter } from './jobs.js';
 import { router as applicationRouter } from './applications.js';
 import { router as resumeRouter } from './resumes.js';
 import { router as reviewRouter } from './reviews.js';
+import { router as searchHistoryRouter } from './searchHistory.js';
 
 export const router = express.Router();
 
@@ -20,3 +21,4 @@ router.use('/jobs', jobRouter);
 router.use('/applications', applicationRouter);
 router.use('/resumes', resumeRouter);
 router.use('/reviews', reviewRouter);
+router.use('/search-history', searchHistoryRouter);
